21e306465be417d926952afbb3367f1f219860b0
Matt Dowds <macmatt99@gmail.com>
2020-12-13 23:50:11 +0000

    Day 10 part 2 (working this time)

--- 2020/Day10.fsx
+++ 2020/Day10.fsx
@@ -22,7 +22,6 @@ let partOneOutput =
     |> Map
     |> (fun map -> Map.find (1) map * Map.find (3) map)
 
-// Attempted solution which takes too long to complete
 let areAdaptersCompatible x y = y - x > 0 && y - x < 4
 
 let getCompatibleAdapters all current =
@@ -32,16 +31,42 @@ let rec buildCombinations (combinationCount: int64) (current: int) (allCompatibl
     match Map.tryFind current allCompatibles with
     | Some compatibleAdapters ->
         match compatibleAdapters with
-        | [] ->
-            combinationCount + 1L
+        | [] -> combinationCount + 1L
         | _ ->
             compatibleAdapters
             |> List.fold (fun count ca -> buildCombinations count ca allCompatibles) combinationCount
     | None -> combinationCount + 1L
 
-let partTwoOutput =
+// Attempted solution which takes too long to complete
+//let partTwoOutput =
+//    adapters
+//    |> (fun rs -> 0 :: rs)
+//    |> List.map (fun a -> (a, getCompatibleAdapters adapters a))
+//    |> Map
+//    |> buildCombinations 0L 0
+
+let addToGroup x groups =
+    let h :: t = groups
+    (h @ [ x ]) :: t
+
+let groupAdapters (adapters: int list) =
+    let processEl (x, y) (groups: int list list) =
+        if y - x = 1 then addToGroup x groups else [ x ] :: groups
+
     adapters
-    |> (fun rs -> 0 :: rs)
+    |> List.pairwise
+    |> (fun list -> List.foldBack processEl list [])
+
+let combinationsForGroup group =
+    group
     |> List.map (fun a -> (a, getCompatibleAdapters adapters a))
     |> Map
-    |> buildCombinations 0L 0
+    |> buildCombinations 0L (List.min group)
+
+let partTwoOutput =
+    adapters
+    |> (fun rs -> rs @ [ 0; List.max rs + 3 ])
+    |> List.sort
+    |> groupAdapters
+    |> List.map combinationsForGroup
+    |> List.reduce (*)
