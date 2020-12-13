930184985713d3ea5fc4bef7af2cf37ce913e96b
Matt Dowds <matthewd99@gmail.com>
2020-12-13 22:35:36 +0000

    Day 10 part 2 (solution that doesn't work)

--- 2020/Day10.fsx
+++ 2020/Day10.fsx
@@ -21,3 +21,27 @@ let partOneOutput =
     |> List.countBy (fun x -> x)
     |> Map
     |> (fun map -> Map.find (1) map * Map.find (3) map)
+
+// Attempted solution which takes too long to complete
+let areAdaptersCompatible x y = y - x > 0 && y - x < 4
+
+let getCompatibleAdapters all current =
+    all |> List.filter (areAdaptersCompatible current)
+
+let rec buildCombinations (combinationCount: int64) (current: int) (allCompatibles: Map<int, int list>) =
+    match Map.tryFind current allCompatibles with
+    | Some compatibleAdapters ->
+        match compatibleAdapters with
+        | [] ->
+            combinationCount + 1L
+        | _ ->
+            compatibleAdapters
+            |> List.fold (fun count ca -> buildCombinations count ca allCompatibles) combinationCount
+    | None -> combinationCount + 1L
+
+let partTwoOutput =
+    adapters
+    |> (fun rs -> 0 :: rs)
+    |> List.map (fun a -> (a, getCompatibleAdapters adapters a))
+    |> Map
+    |> buildCombinations 0L 0
